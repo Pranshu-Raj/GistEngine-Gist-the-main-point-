9307cc94eedb6d52e3f214ceca59ff61fb69528d
Pranshu Raj <pranshuraj828@gmail.com>
2025-11-01 23:04:24 +0530

    feat: add article summarization feature with UI components

---
+++ src/components/Demo.jsx
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+
+import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
+
+function Demo() {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    
+  })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updateAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updateAllArticles)
+      // console.log(newArticle);
+    }
+  };
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          action=""
+          className="relative flex justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer"
+          />
+
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            ↩️
+          </button>
+        </form>
+
+        {/* Browse URL History */}
+      </div>
+      {/* Display results */}
+    </section>
+  );
+}
+
+export default Demo;
